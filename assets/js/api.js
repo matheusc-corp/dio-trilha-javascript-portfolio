2b4e83642660626674ca2a591b682a0dc472d85d
Matheus Carvalho <matheus.carvalho.corp@gmail.com>
2026-02-14 17:04:22 +0900

    Fix: ajuste de rotas

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/matheusc-corp/dio-trilha-javascript-portfolio/refs/heads/main/assets/json/profile.json'
+    const url = 'https://raw.githubusercontent.com/matheusc-corp/dio-trilha-javascript-portfolio/refs/heads/main/assets/data/profile.json'
     const fetching = await fetch(url)
 
     return await fetching.json();
